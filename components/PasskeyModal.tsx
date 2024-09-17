a39bc4db89593926395ecb77bbf946ca8981cdd6
Zayd <zaidbaig738@gmail.com>
2024-09-17 12:23:59 +0530

    Fixed admin Bug
    Fixed the bug where admin as not loading directly and had to be reloaded manually

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -3,13 +3,11 @@
 import {
   InputOTP,
   InputOTPGroup,
-  InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
 import {
   AlertDialog,
   AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
@@ -21,38 +19,48 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { decryptKey, encryptKey } from "@/lib/utils";
 
-const PasskeyModal = () => {
+export const PasskeyModal = () => {
   const router = useRouter();
   const path = usePathname();
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const [passkey, setPasskey] = useState("");
   const [error, setError] = useState("");
+
+  const encryptedKey =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("accessKey")
+      : null;
+
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+    if (path)
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY!.toString()) {
+        setOpen(false);
+        router.push("/admin");
+      } else {
+        setOpen(true);
+      }
+  }, [encryptedKey]);
 
   const closeModal = () => {
     setOpen(false);
     router.push("/");
   };
 
-  useEffect(() => {
-    const encryptedKey = typeof window !== "undefined" ? window.localStorage.getItem("accessKey") : null;
-    const accessKey = encryptedKey && decryptKey(encryptedKey);
-
-    if (path && accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
-      router.push("/admin");
-      setOpen(false);
-    } else {
-      setOpen(true);
-    }
-  }, [path]);
-
-  const validatePasskey = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const validatePasskey = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
+
     if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
       const encryptedKey = encryptKey(passkey);
+
       localStorage.setItem("accessKey", encryptedKey);
+
       setOpen(false);
     } else {
-      setError("Invalid Passkey, Please try again");
+      setError("Invalid passkey. Please try again.");
     }
   };
 
@@ -67,7 +75,7 @@ const PasskeyModal = () => {
               alt="close"
               width={20}
               height={20}
-              onClick={closeModal}
+              onClick={() => closeModal()}
               className="cursor-pointer"
             />
           </AlertDialogTitle>
@@ -76,7 +84,11 @@ const PasskeyModal = () => {
           </AlertDialogDescription>
         </AlertDialogHeader>
         <div>
-          <InputOTP maxLength={6} value={passkey} onChange={(value) => setPasskey(value)}>
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPasskey(value)}
+          >
             <InputOTPGroup className="shad-otp">
               <InputOTPSlot className="shad-otp-slot" index={0} />
               <InputOTPSlot className="shad-otp-slot" index={1} />
@@ -86,6 +98,7 @@ const PasskeyModal = () => {
               <InputOTPSlot className="shad-otp-slot" index={5} />
             </InputOTPGroup>
           </InputOTP>
+
           {error && (
             <p className="shad-error text-14-regular mt-4 flex justify-center">
               {error}
@@ -94,8 +107,8 @@ const PasskeyModal = () => {
         </div>
         <AlertDialogFooter>
           <AlertDialogAction
-            className="shad-primary-btn w-full"
             onClick={(e) => validatePasskey(e)}
+            className="shad-primary-btn w-full"
           >
             Enter Admin Passkey
           </AlertDialogAction>
@@ -104,5 +117,3 @@ const PasskeyModal = () => {
     </AlertDialog>
   );
 };
-
-export default PasskeyModal;
